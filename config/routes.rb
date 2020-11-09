f377c9354bbf2b85231ce8eff53e059a823c7599
Martin Slemr <mslemr@redhat.com>
2020-11-09 13:08:44 +0100

    env based routes prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  resources :queries, :only => %i[index create]
+  prefix = "app"
+  if ENV["PATH_PREFIX"].present? && ENV["APP_NAME"].present?
+    prefix = File.join(ENV["PATH_PREFIX"], ENV["APP_NAME"]).gsub(/^\/+|\/+$/, "")
+  end
+
+  scope :as => :app, :path => prefix do
+    root :to => 'queries#index'
+    post '/', :to => 'queries#create'
+  end
 end
